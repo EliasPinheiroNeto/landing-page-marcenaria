e0e640f3f470b57bc472fbee68d196225f8f546c
Elias Pinheiro <pinheirinho2004@gmail.com>
2023-11-18 00:59:41 -0300

    gallery section

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import Intro from './sections/Intro.vue'
 import About from './sections/About.vue'
+import Gallery from './sections/Gallery.vue'
 </script> 
 
 <template>
   <Intro />
+
   <About />
 
-  <!-- About Section -->
-  <!-- Gallery Section -->
+  <Gallery />
 
   <!-- Footer -->
 </template> 
---
+++ src/sections/Gallery.vue
@@ -0,0 +1,31 @@
+<script setup></script> 
+
+<template>
+    <div class="bg-blue-400">
+        <section class="container mx-auto grid grid-cols-4 grid-rows-2 gap-4 py-12 px-16">
+            <img class="w-full aspect-square object-cover hover:scale-110 transition-transform"
+                 src="https://placehold.co/400x600?text=Imagem"
+                 alt="">
+            <img class="w-full aspect-square object-cover hover:scale-110 transition-transform"
+                 src="https://placehold.co/400x600?text=Imagem"
+                 alt="">
+            <img class="w-full aspect-square object-cover hover:scale-110 transition-transform"
+                 src="https://placehold.co/400x600?text=Imagem"
+                 alt="">
+            <img class="w-full aspect-square object-cover hover:scale-110 transition-transform"
+                 src="https://placehold.co/400x600?text=Imagem"
+                 alt="">
+            <img class="w-full aspect-square object-cover hover:scale-110 transition-transform"
+                 src="https://placehold.co/400x600?text=Imagem"
+                 alt="">
+            <img class="w-full aspect-square object-cover hover:scale-110 transition-transform"
+                 src="https://placehold.co/400x600?text=Imagem"
+                 alt="">
+            <img class="w-full aspect-square object-cover hover:scale-110 transition-transform"
+                 src="https://placehold.co/400x600?text=Imagem"
+                 alt="">
+        </section>
+    </div>
+</template> 
+
+<style scoped></style> 
