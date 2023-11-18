19b27a02a275ea19d11a9260d6be01eb6aead70c
Elias Pinheiro <pinheirinho2004@gmail.com>
2023-11-18 01:11:48 -0300

    minor improvements

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,14 +1,14 @@
 <script setup></script> 
 
 <template>
-    <section class="container 2xl:mx-auto grid grid-cols-5">
-        <div class="col-span-2">
+    <section class="container 2xl:mx-auto grid grid-cols-7">
+        <div class="col-span-3">
             <img class="block h-full object-cover"
                  src="https://placehold.co/400?text=Banner"
                  alt="">
         </div>
 
-        <div class="grid grid-rows-2 col-span-3 px-8 py-16 text-white">
+        <div class="grid grid-rows-2 col-span-4 px-8 py-16 text-white">
             <!-- Texto forte -->
             <div>
                 <h1 class="text-6xl">Transforme <br><span class="text-3xl">a sua <strong>casa</strong></span></h1>
