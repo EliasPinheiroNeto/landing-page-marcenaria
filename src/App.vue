b695ff2731c391a8d66f717c18c7e141fe3dd551
Elias Pinheiro <pinheirinho2004@gmail.com>
2023-11-18 00:48:36 -0300

    about section

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Intro from './sections/Intro.vue'
+import About from './sections/About.vue'
 </script> 
 
 <template>
   <Intro />
+  <About />
 
   <!-- About Section -->
   <!-- Gallery Section -->
--- src/sections/About.vue
+++ src/sections/About.vue
@@ -1,17 +1,24 @@
 <script setup></script> 
 
 <template>
-    <div class="w-screen">
-        <section class="grid grid-cols-2">
-            <div>
-                <h2>Quem Somos?</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum numquam accusantium laboriosam quam ut
-                    ullam, delectus impedit minima asperiores illo eligendi consequatur nihil eaque provident autem sit
-                    necessitatibus blanditiis quo?</p>
+    <div class="w-full">
+        <section class="grid grid-cols-2 container mx-auto">
+            <div class="p-8 my-auto">
+                <h2 class="text-4xl">Quem Somos?</h2>
+                <p class="mt-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias optio vitae voluptates earum
+                    voluptate commodi ipsum eius! Voluptate eum magni aspernatur laborum architecto possimus minima,
+                    asperiores, exercitationem inventore, suscipit repellendus. Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Amet, est asperiores nesciunt reiciendis eius eligendi nulla quam sit voluptas nisi
+                    eaque rem esse, doloremque quis magni. Neque similique sunt porro. Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda
+                    recusandae ducimus illo itaque incidunt quo quidem ut. Molestias, inventore rem ipsam officiis fugit
+                    eligendi, ad, blanditiis numquam voluptatum earum repellendus.</p>
             </div>
 
-            <div>
-                <img src="https://placehold.co/400x600?text=Imagem"
+            <div class="py-8 relative flex justify-center">
+                <div class="absolute w-2/3 aspect-square border-4 border-blue-400 -translate-x-4 translate-y-4"></div>
+                <img class="w-2/3 aspect-square object-cover"
+                     src="https://placehold.co/400x600?text=Imagem"
                      alt="">
             </div>
         </section>
