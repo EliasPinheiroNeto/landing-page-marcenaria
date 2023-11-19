6dea2dc98c7e6eaf5a8a45e70396679e5cad4da2
Elias Pinheiro <pinheirinho2004@gmail.com>
2023-11-19 19:10:23 -0300

    google maps

--- src/sections/Maps.vue
+++ src/sections/Maps.vue
@@ -2,9 +2,12 @@
 
 <template>
     <div class="w-full">
-        <section class="container mx-auto">
-            <img src="https://placehold.co/800x400?text=Maps"
-                 alt="">
+        <section class="2xl:container mx-auto">
+            <iframe class="w-full"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3660.1040314049937!2d-46.532933089835986!3d-23.4567116787838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef53f5e6df39b%3A0xfe878b6bf11d195b!2sParque%20Bosque%20Maia!5e0!3m2!1spt-BR!2sbr!4v1700431592247!5m2!1spt-BR!2sbr"
+                    height="400"
+                    allowfullscreen="false"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
         </section>
     </div>
 </template> 
